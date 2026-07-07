b12522e2aabb5e72f6a3b585d8ae2a04e9da4258
FRAME-890 <framef590@gmail.com>
2026-07-07 19:47:12 +0700

    Add Supabase configuration for client initialization

--- supabase-config.js
+++ supabase-config.js
@@ -1,1 +1,4 @@
-
+// js/supabase-config.js
+const supabaseUrl = 'https://abgtfwjsauobbbnywdts.supabase.co';
+const supabaseKey = 'sb_publishable_3nA8LZf5Y0C03JzCEMkYcQ_BxXEpOB0';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
