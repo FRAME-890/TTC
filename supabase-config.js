a31991a317ee12692854c37402511791134ef1f1
FRAME-890 <framef590@gmail.com>
2026-07-07 22:01:57 +0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-// js/supabase-config.js
-const supabaseUrl = 'https://abgtfwjsauobbbnywdts.supabase.co';
-const supabaseKey = 'sb_publishable_3nA8LZf5Y0C03JzCEMkYcQ_BxXEpOB0';
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
